437f87de842083d33685725fa51fb81da93d85b5
yujianchao <yujianchao@hupu.com>
2026-08-18 11:18:26 +0800

    Initialize LampsSDK pod skeleton

---
+++ Example/Pods/Target Support Files/LampsSDK/LampsSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LampsSDK : NSObject
+@end
+@implementation PodsDummy_LampsSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-LampsSDK_Example/Pods-LampsSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LampsSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LampsSDK_Example
+@end
