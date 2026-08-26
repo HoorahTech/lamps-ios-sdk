ec624e3987e9acdcd2dc688681672f5eba87f187
yujianchao <yujianchao@hupu.com>
2026-08-26 14:43:34 +0800

    fix：修改.gitignore

--- Example/Pods/Target Support Files/LampsSDK/LampsSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LampsSDK : NSObject
-@end
-@implementation PodsDummy_LampsSDK
-@end
--- Example/Pods/Target Support Files/Pods-LampsSDK_Example/Pods-LampsSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LampsSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_LampsSDK_Example
-@end
